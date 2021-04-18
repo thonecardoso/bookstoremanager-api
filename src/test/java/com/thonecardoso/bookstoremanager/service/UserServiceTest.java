7d5eea08c39eb8bc8e8661295c6181cb668e0db8
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2021-04-18 16:21:49 -0300

    Developed user creation feature

--- src/test/java/com/thonecardoso/bookstoremanager/service/UserServiceTest.java
+++ src/test/java/com/thonecardoso/bookstoremanager/service/UserServiceTest.java
@@ -1,6 +1,10 @@
 package com.thonecardoso.bookstoremanager.service;
 
 import com.thonecardoso.bookstoremanager.users.builder.UserDTOBuilder;
+import com.thonecardoso.bookstoremanager.users.dto.MessageDTO;
+import com.thonecardoso.bookstoremanager.users.dto.UserDTO;
+import com.thonecardoso.bookstoremanager.users.entity.User;
+import com.thonecardoso.bookstoremanager.users.exception.UserAlreadyExistsException;
 import com.thonecardoso.bookstoremanager.users.mapper.UserMapper;
 import com.thonecardoso.bookstoremanager.users.repository.UserRepository;
 import com.thonecardoso.bookstoremanager.users.service.UserService;
@@ -10,6 +14,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -26,8 +38,35 @@ public class UserServiceTest {
 
     @BeforeEach
     void setUp() {
-        UserDTOBuilder.builder().build();
+        userDTOBuilder = UserDTOBuilder.builder().build();
     }
 
+    @Test
+    void whenNewUserIsInformedItShouldBeCreated() {
+        UserDTO expectedCreatedUserDTO = userDTOBuilder.buildUserDTO();
+        User expectedCreatedUser = userMapper.toModel(expectedCreatedUserDTO);
+        String expectedCreationMessage = "User thonecardoso with ID 1 successfully created";
+        String expectedUsername = expectedCreatedUserDTO.getUsername();
+        String expecteUserEmail = expectedCreatedUserDTO.getEmail();
+
+        when(userRepository.findByUsernameOrEmail(expectedUsername, expecteUserEmail)).thenReturn(Optional.empty());
+        when(userRepository.save(expectedCreatedUser)).thenReturn(expectedCreatedUser);
+
+        MessageDTO creationMessage = userService.create(expectedCreatedUserDTO);
+
+        assertThat(expectedCreationMessage, is(equalTo(creationMessage.getMessage())));
+    }
+
+    @Test
+    void whenExistingUserIsInformedThenAnExceptionShouldBeThrown() {
+        UserDTO expectedDuplicatedUserDTO = userDTOBuilder.buildUserDTO();
+        User expectedDuplicatedUser = userMapper.toModel(expectedDuplicatedUserDTO);
+        String expectedUsername = expectedDuplicatedUserDTO.getUsername();
+        String expecteUserEmail = expectedDuplicatedUserDTO.getEmail();
+
+        when(userRepository.findByUsernameOrEmail(expectedUsername, expecteUserEmail)).thenReturn(Optional.of(expectedDuplicatedUser));
+
+        assertThrows(UserAlreadyExistsException.class, ()->userService.create(expectedDuplicatedUserDTO));
+    }
 
 }
