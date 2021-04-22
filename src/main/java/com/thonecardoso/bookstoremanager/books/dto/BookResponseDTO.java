72bc9eab0dd7cbf898356759bb4b7417a0b324d5
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2021-04-22 06:12:20 -0300

    Created Book DTOs for requests and responses

--- src/main/java/com/thonecardoso/bookstoremanager/books/dto/BookResponseDTO.java
+++ src/main/java/com/thonecardoso/bookstoremanager/books/dto/BookResponseDTO.java
@@ -1,2 +1,27 @@
-package com.thonecardoso.bookstoremanager.books.dto;public class BookResponseDTO {
+package com.thonecardoso.bookstoremanager.books.dto;
+
+import com.thonecardoso.bookstoremanager.author.dto.AuthorDTO;
+import com.thonecardoso.bookstoremanager.publishers.dto.PublisherDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookResponseDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String isbn;
+
+    private Long pages;
+
+    private Long chapters;
+
+    private AuthorDTO author;
+
+    private PublisherDTO publisher;
 }
